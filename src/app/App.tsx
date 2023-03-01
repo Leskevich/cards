76a9ce1fcebc6525e9371e70098487ba59e0fcad
leskevich <leskevichtema@gmail.com>
2023-03-01 23:47:01 +0300

    create HeaderComponent

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Pages } from "./pages/pages";
+import { Header } from "../features/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Pages />
     </div>
   );

e36b2a2c72e43355fec9240db077f14b43ff7444
Samuil Prytchyn <79350951+000004-dj@users.noreply.github.com>
2023-03-07 23:35:40 +0100

    fix: add small changes

--- src/features/auth/auth-slice.ts
+++ src/features/auth/auth-slice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Dispatch } from "redux";
-import {authAPI, ForgotPasswordType, loginResponseType, RegisterPayloadType} from "../../api/auth-api";
+import { authAPI, ForgotPasswordType, loginResponseType, RegisterPayloadType } from "../../api/auth-api";
 import { setAppStatusAC, setIsInitializedAC } from "../../app/app-reducer";
 import { setProfile } from "../Profile/profile-slice";
 import { ErrorNetwork } from "../../common/utils/ErrorNetwork";
@@ -9,7 +9,7 @@ const initialState = {
   isLoggedIn: false,
   isRegister: false,
   isEMail: false,
-  email: "",
+  email: ""
 };
 
 const slice = createSlice({
@@ -27,8 +27,8 @@ const slice = createSlice({
     },
     setMail(state, action: PayloadAction<{ email: string }>) {
       state.email = action.payload.email;
-    },
-  },
+    }
+  }
 });
 
 export const { setIsLoggedIn, setIsRegistration, setIsMail, setMail } = slice.actions;
@@ -42,7 +42,7 @@ export const forgot = (email: string) => async (dispatch: Dispatch) => {
     message: `<div style="background-color: lime; padding: 15px">
 password recovery link:   
 <a href="http://localhost:3000/cards#/newPassword/$token$">link</a>
-</div>`,
+</div>`
   };
   try {
     await authAPI.forgotPassword(data);
@@ -52,6 +52,7 @@ password recovery link:
     ErrorNetwork(e, dispatch);
   }
 };
+
 export const login = (data: loginResponseType) => async (dispatch: Dispatch) => {
   dispatch(setAppStatusAC({ status: "loading" }));
   try {
@@ -59,23 +60,17 @@ export const login = (data: loginResponseType) => async (dispatch: Dispatch) => 
     if (response) {
       dispatch(setIsLoggedIn({ isLoginIn: true }));
     }
-    dispatch(setAppStatusAC({ status: "succeeded" }));
   } catch (e) {
     ErrorNetwork(e, dispatch);
-  }finally {
+  } finally {
     dispatch(setAppStatusAC({ status: "idle" }));
   }
 };
 export const logoutThunk = () => async (dispatch: Dispatch) => {
+  dispatch(setAppStatusAC({ status: "loading" }));
   try {
-    dispatch(setAppStatusAC({ status: "loading" }));
-    const res = await authAPI.logOut();
-    if (!res.data.error) {
-      dispatch(setIsLoggedIn({ isLoginIn: false }));
-      dispatch(setAppStatusAC({ status: "succeeded" }));
-    } else {
-      alert("Are you sure about you are programmer?");
-    }
+    await authAPI.logOut();
+    dispatch(setIsLoggedIn({ isLoginIn: false }));
   } catch (e) {
     ErrorNetwork(e, dispatch);
   } finally {
@@ -83,8 +78,8 @@ export const logoutThunk = () => async (dispatch: Dispatch) => {
   }
 };
 export const registerTC = (data: RegisterPayloadType) => async (dispatch: Dispatch) => {
+  dispatch(setAppStatusAC({ status: "loading" }));
   try {
-    dispatch(setAppStatusAC({ status: "loading" }));
     await authAPI.register(data);
     dispatch(setAppStatusAC({ status: "idle" }));
     dispatch(setIsRegistration({ isRegister: true }));
@@ -96,13 +91,13 @@ export const registerTC = (data: RegisterPayloadType) => async (dispatch: Dispat
 };
 export const setNewUserNameThunk = (title: string, avatar?: string) => async (dispatch: Dispatch) => {
   try {
-    dispatch(setAppStatusAC({ status: "loading" }))
+    dispatch(setAppStatusAC({ status: "loading" }));
     const res = await authAPI.setNewUserName(title, avatar);
     dispatch(setProfile(res.data.updatedUser));
   } catch (e: any) {
     ErrorNetwork(e, dispatch);
-  }finally {
-    dispatch(setAppStatusAC({ status: "idle" }))
+  } finally {
+    dispatch(setAppStatusAC({ status: "idle" }));
   }
 };
 export const isAuth = () => async (dispatch: Dispatch) => {

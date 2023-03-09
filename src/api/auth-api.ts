962c8349c7dbc62c3ec835217d6a0e8fdb416751
leskevich <leskevichtema@gmail.com>
2023-03-09 14:50:45 +0300

    fix:authApiType

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,81 +1,70 @@
-import { AxiosResponse } from "axios";
-import { instance } from "../common/instance/instance";
+import { AxiosResponse } from "axios"
+import { instance } from "../common/instance/instance"
 
 export const authAPI = {
-  login(payload: loginResponseType) {
+  register(payload: RegisterPayloadType) {
+    return instance.post("https://neko-back.herokuapp.com/2.0/auth/register", payload).then((res) => res.data)
+  },
+  login(payload: LoginPayloadType) {
     return instance
-      .post<any, AxiosResponse<LoginRequestType>, loginResponseType>("https://neko-back.herokuapp.com/2.0/auth/login", payload)
-      .then((res) => res.data);
+      .post<any, AxiosResponse<ProfileType>, LoginPayloadType>(
+        "https://neko-back.herokuapp.com/2.0/auth/login",
+        payload
+      )
+      .then((res) => res.data)
   },
   logOut() {
-    return instance.delete<{ info: string; error: string }>("https://neko-back.herokuapp.com/2.0/auth/me");
+    return instance.delete<{ info: string }>("https://neko-back.herokuapp.com/2.0/auth/me")
   },
   me() {
-    return instance.post<LoginRequestType>("https://neko-back.herokuapp.com/2.0/auth/me", {});
+    return instance.post("https://neko-back.herokuapp.com/2.0/auth/me", {})
   },
-  setNewUserName(name: string, avatar?: string) {
-    return instance.put<any, AxiosResponse<UpdateResponseUserType>, ChangeNameAvatarType>("https://neko-back.herokuapp.com/2.0/auth/me", {name, avatar});
+  setNewUserName(payload: UpdateUserType) {
+    return instance.put<any, AxiosResponse<UpdateResponseUserType>, UpdateUserType>(
+      "https://neko-back.herokuapp.com/2.0/auth/me",
+      payload
+    )
   },
-  register(payload: RegisterPayloadType) {
-    return instance.post<any, AxiosResponse<RegisterResponseType>, RegisterPayloadType>("https://neko-back.herokuapp.com/2.0/auth/register", payload)
-      .then((res) => res.data);
+  forgotPassword(payload: ForgotPasswordType) {
+    return instance.post<AxiosResponse<{ info: string }>>("https://neko-back.herokuapp.com/2.0/auth/forgot", payload)
   },
-  forgotPassword(data: ForgotPasswordType) {
-    return instance.post("https://neko-back.herokuapp.com/2.0/auth/forgot", data);
+  setNewPassword(payload: SetNewPasswordType) {
+    return instance.post<AxiosResponse<{ info: string }>>(
+      "https://neko-back.herokuapp.com/2.0/auth/set-new-password",
+      payload
+    )
   },
-  setNewPassword(data: SetNewPasswordType){
-    return instance.post<AxiosResponse<{info: string, error: string}>>("https://neko-back.herokuapp.com/2.0/auth/set-new-password", data)
-  }
-};
+}
 
 export type SetNewPasswordType = {
-  password: string,
+  password: string
   resetPasswordToken: string | undefined
 }
 export type ForgotPasswordType = {
-  email: string;
-  from: string;
-  message: string;
-};
-export type ChangeNameAvatarType = {
-  name: string;
-  avatar?: string;
-};
-export type RegisterPayloadType = {
-  email: string;
-  password: string;
-};
-
-export type loginResponseType = {
-  email: string;
-  password: string;
-  rememberMe: boolean;
-};
-
-export type UserType = {
-  _id?: string;
-  email: string;
-  rememberMe?: boolean;
-  isAdmin?: boolean;
-  name: string;
-  verified?: boolean;
-  publicCardPacksCount?: number;
-  created?: string;
-  updated?: string;
-  __v?: number;
-};
-
-type RegisterResponseType = {
-  addedUser: UserType;
-  error?: string
-};
-export type LoginRequestType = UserType & {
-  token: string;
-  tokenDeathTime: number;
-  avatar: string;
-};
+  email: string
+  from: string
+  message: string
+}
 type UpdateResponseUserType = {
-  updatedUser: LoginRequestType;
-  token: string;
-  tokenDeathTime: number;
-};
+  updatedUser: ProfileType
+  token: string
+  tokenDeathTime: number
+}
+export type UpdateUserType = {
+  name?: string
+  avatar?: string
+}
+export type RegisterPayloadType = {
+  email: string
+  password: string
+}
+export type LoginPayloadType = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
+type ProfileType = {
+  email: string
+  name: string
+  avatar: string
+}
--- src/features/auth/auth-slice.ts
+++ src/features/auth/auth-slice.ts
@@ -1,48 +1,48 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {Dispatch} from "redux";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { Dispatch } from "redux"
 import {
   authAPI,
   ForgotPasswordType,
-  loginResponseType,
+  LoginPayloadType,
   RegisterPayloadType,
-  SetNewPasswordType
-} from "../../api/auth-api";
-import {setAppErrorAC, setAppStatusAC, setIsInitializedAC} from "../../app/app-slice";
-import {setProfile} from "../Profile/profile-slice";
-import {ErrorNetwork} from "../../common/utils/ErrorNetwork";
+  SetNewPasswordType,
+} from "../../api/auth-api"
+import { setAppErrorAC, setAppStatusAC, setIsInitializedAC } from "../../app/app-slice"
+import { setProfile } from "../Profile/profile-slice"
+import { ErrorNetwork } from "../../common/utils/ErrorNetwork"
 
 const initialState = {
   isLoggedIn: false,
   isRegister: false,
   isEMail: false,
   email: "",
-  isChangePassword: false
-};
+  isChangePassword: false,
+}
 
 const slice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setIsLoggedIn(state, action: PayloadAction<{ isLoginIn: boolean }>) {
-      state.isLoggedIn = action.payload.isLoginIn;
+      state.isLoggedIn = action.payload.isLoginIn
     },
     setIsRegistration(state, action: PayloadAction<{ isRegister: boolean }>) {
-      state.isRegister = action.payload.isRegister;
+      state.isRegister = action.payload.isRegister
     },
     setIsMail(state, action: PayloadAction<{ isEMail: boolean }>) {
-      state.isEMail = action.payload.isEMail;
+      state.isEMail = action.payload.isEMail
     },
     setMail(state, action: PayloadAction<{ email: string }>) {
-      state.email = action.payload.email;
+      state.email = action.payload.email
     },
     changePassword(state, action: PayloadAction<{ isChangePassword: boolean }>) {
       state.isChangePassword = action.payload.isChangePassword
-    }
-  }
-});
+    },
+  },
+})
 
-export const {setIsLoggedIn, setIsRegistration, setIsMail, setMail, changePassword} = slice.actions;
-export const authSlice = slice.reducer;
+export const { setIsLoggedIn, setIsRegistration, setIsMail, setMail, changePassword } = slice.actions
+export const authSlice = slice.reducer
 
 //Thunks
 export const forgot = (email: string) => async (dispatch: Dispatch) => {
@@ -52,87 +52,86 @@ export const forgot = (email: string) => async (dispatch: Dispatch) => {
     message: `<div style="background-color: lime; padding: 15px">
 password recovery link:   
 <a href="http://localhost:3000/cards#/newPassword/$token$">link</a>
-</div>`
-  };
-  try {
-    await authAPI.forgotPassword(data);
-    dispatch(setIsMail({isEMail: true}));
-    dispatch(setMail({email}));
-  } catch (e) {
-    ErrorNetwork(e, dispatch);
+</div>`,
   }
-};
-
-export const login = (data: loginResponseType) => async (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC({status: "loading"}));
   try {
-    const response = await authAPI.login(data);
-    if (response) {
-      dispatch(setIsLoggedIn({isLoginIn: true}));
-    }
+    await authAPI.forgotPassword(data)
+    dispatch(setIsMail({ isEMail: true }))
+    dispatch(setMail({ email }))
   } catch (e) {
-    ErrorNetwork(e, dispatch);
-  } finally {
-    dispatch(setAppStatusAC({status: "idle"}));
-  }
-};
-export const logoutThunk = () => async (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC({status: "loading"}));
-  try {
-    await authAPI.logOut();
-    dispatch(setIsLoggedIn({isLoginIn: false}));
-  } catch (e) {
-    ErrorNetwork(e, dispatch);
-  } finally {
-    dispatch(setAppStatusAC({status: "idle"}));
-  }
-};
-export const registerTC = (data: RegisterPayloadType) => async (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC({status: "loading"}));
-  try {
-    await authAPI.register(data);
-    dispatch(setAppStatusAC({status: "idle"}));
-    dispatch(setIsRegistration({isRegister: true}));
-  } catch (e) {
-    ErrorNetwork(e, dispatch);
-  } finally {
-    dispatch(setAppStatusAC({status: "idle"}));
-  }
-};
-export const setNewUserNameThunk = (title: string, avatar?: string) => async (dispatch: Dispatch) => {
-  try {
-    dispatch(setAppStatusAC({status: "loading"}));
-    const res = await authAPI.setNewUserName(title, avatar);
-    dispatch(setProfile(res.data.updatedUser));
-  } catch (e) {
-    ErrorNetwork(e, dispatch);
-  } finally {
-    dispatch(setAppStatusAC({status: "idle"}));
-  }
-};
-export const isAuth = () => async (dispatch: Dispatch) => {
-  try {
-    dispatch(setAppStatusAC({status: "loading"}));
-    const res = await authAPI.me();
-    dispatch(setIsLoggedIn({isLoginIn: true}));
-    dispatch(setProfile(res.data));
-  } catch (e) {
-    ErrorNetwork(e, dispatch);
-  } finally {
-    dispatch(setAppStatusAC({status: "idle"}));
-    dispatch(setIsInitializedAC({isInitialized: true}));
-  }
-};
-export const changePasswordTC = (data: SetNewPasswordType) => async (dispatch: Dispatch) => {
-  try{
-    dispatch(setAppStatusAC({status: "loading"}));
-    await authAPI.setNewPassword(data)
-    dispatch(changePassword({isChangePassword: true}))
-    dispatch(setAppErrorAC({error: 'Your password changed'}))
-  } catch (e){
     ErrorNetwork(e, dispatch)
   }
-  finally {
-    dispatch(setAppStatusAC({status: "idle"}));
+}
+
+export const login = (data: LoginPayloadType) => async (dispatch: Dispatch) => {
+  dispatch(setAppStatusAC({ status: "loading" }))
+  try {
+    const response = await authAPI.login(data)
+    if (response) {
+      dispatch(setIsLoggedIn({ isLoginIn: true }))
+    }
+  } catch (e) {
+    ErrorNetwork(e, dispatch)
+  } finally {
+    dispatch(setAppStatusAC({ status: "idle" }))
+  }
+}
+export const logoutThunk = () => async (dispatch: Dispatch) => {
+  dispatch(setAppStatusAC({ status: "loading" }))
+  try {
+    await authAPI.logOut()
+    dispatch(setIsLoggedIn({ isLoginIn: false }))
+  } catch (e) {
+    ErrorNetwork(e, dispatch)
+  } finally {
+    dispatch(setAppStatusAC({ status: "idle" }))
+  }
+}
+export const registerTC = (data: RegisterPayloadType) => async (dispatch: Dispatch) => {
+  dispatch(setAppStatusAC({ status: "loading" }))
+  try {
+    await authAPI.register(data)
+    dispatch(setAppStatusAC({ status: "idle" }))
+    dispatch(setIsRegistration({ isRegister: true }))
+  } catch (e) {
+    ErrorNetwork(e, dispatch)
+  } finally {
+    dispatch(setAppStatusAC({ status: "idle" }))
+  }
+}
+export const setNewUserNameThunk = (name: string, avatar?: string) => async (dispatch: Dispatch) => {
+  try {
+    dispatch(setAppStatusAC({ status: "loading" }))
+    const res = await authAPI.setNewUserName({ name, avatar })
+    dispatch(setProfile(res.data.updatedUser))
+  } catch (e) {
+    ErrorNetwork(e, dispatch)
+  } finally {
+    dispatch(setAppStatusAC({ status: "idle" }))
+  }
+}
+export const isAuth = () => async (dispatch: Dispatch) => {
+  try {
+    dispatch(setAppStatusAC({ status: "loading" }))
+    const res = await authAPI.me()
+    dispatch(setIsLoggedIn({ isLoginIn: true }))
+    dispatch(setProfile(res.data))
+  } catch (e) {
+    ErrorNetwork(e, dispatch)
+  } finally {
+    dispatch(setAppStatusAC({ status: "idle" }))
+    dispatch(setIsInitializedAC({ isInitialized: true }))
+  }
+}
+export const changePasswordTC = (data: SetNewPasswordType) => async (dispatch: Dispatch) => {
+  try {
+    dispatch(setAppStatusAC({ status: "loading" }))
+    await authAPI.setNewPassword(data)
+    dispatch(changePassword({ isChangePassword: true }))
+    dispatch(setAppErrorAC({ error: "Your password changed" }))
+  } catch (e) {
+    ErrorNetwork(e, dispatch)
+  } finally {
+    dispatch(setAppStatusAC({ status: "idle" }))
   }
 }

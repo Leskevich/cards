1995db8affa7e0a5310d681f38edadb043d00222
leskevich <leskevichtema@gmail.com>
2023-03-10 15:45:40 +0300

    fix-refactor instance

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -1,70 +1,66 @@
-import { AxiosResponse } from "axios"
-import { instance } from "../common/instance/instance"
+import {AxiosResponse} from "axios"
+import {instanceHeroku} from "../common/instance/instance"
 
 export const authAPI = {
-  register(payload: RegisterPayloadType) {
-    return instance.post("https://neko-back.herokuapp.com/2.0/auth/register", payload).then((res) => res.data)
-  },
-  login(payload: LoginPayloadType) {
-    return instance
-      .post<any, AxiosResponse<ProfileType>, LoginPayloadType>(
-        "https://neko-back.herokuapp.com/2.0/auth/login",
-        payload
-      )
-      .then((res) => res.data)
-  },
-  logOut() {
-    return instance.delete<{ info: string }>("https://neko-back.herokuapp.com/2.0/auth/me")
-  },
-  me() {
-    return instance.post("https://neko-back.herokuapp.com/2.0/auth/me", {})
-  },
-  setNewUserName(payload: UpdateUserType) {
-    return instance.put<any, AxiosResponse<UpdateResponseUserType>, UpdateUserType>(
-      "https://neko-back.herokuapp.com/2.0/auth/me",
-      payload
-    )
-  },
-  forgotPassword(payload: ForgotPasswordType) {
-    return instance.post<AxiosResponse<{ info: string }>>("https://neko-back.herokuapp.com/2.0/auth/forgot", payload)
-  },
-  setNewPassword(payload: SetNewPasswordType) {
-    return instance.post<AxiosResponse<{ info: string }>>(
-      "https://neko-back.herokuapp.com/2.0/auth/set-new-password",
-      payload
-    )
-  },
+    register(payload: RegisterPayloadType) {
+        return instanceHeroku.post("auth/register", payload).then((res) => res.data)
+    },
+    login(payload: LoginPayloadType) {
+        return instanceHeroku.post<any, AxiosResponse<ProfileType>, LoginPayloadType>("auth/login", payload)
+            .then((res) => res.data)
+    },
+    logOut() {
+        return instanceHeroku.delete<{ info: string }>("auth/me")
+    },
+    me() {
+        return instanceHeroku.post("auth/me", {})
+    },
+    setNewUserName(payload: UpdateUserType) {
+        return instanceHeroku.put<any, AxiosResponse<UpdateResponseUserType>, UpdateUserType>(
+            "auth/me",
+            payload
+        )
+    },
+    forgotPassword(payload: ForgotPasswordType) {
+        return instanceHeroku.post<AxiosResponse<{ info: string }>>("auth/forgot", payload)
+    },
+    setNewPassword(payload: SetNewPasswordType) {
+        return instanceHeroku.post<AxiosResponse<{ info: string }>>(
+            "auth/set-new-password",
+            payload
+        )
+    },
 }
 
 export type ProfileType = {
-  email: string
-  name: string
-  avatar: string
+    email: string
+    name: string
+    avatar: string
 }
 export type RegisterPayloadType = {
-  email: string
-  password: string
+    email: string
+    password: string
 }
 export type LoginPayloadType = {
-  email: string
-  password: string
-  rememberMe: boolean
+    email: string
+    password: string
+    rememberMe: boolean
 }
 export type UpdateUserType = {
-  name?: string
-  avatar?: string
+    name?: string
+    avatar?: string
 }
 type UpdateResponseUserType = {
-  updatedUser: ProfileType
-  token: string
-  tokenDeathTime: number
+    updatedUser: ProfileType
+    token: string
+    tokenDeathTime: number
 }
 export type ForgotPasswordType = {
-  email: string
-  from: string
-  message: string
+    email: string
+    from: string
+    message: string
 }
 export type SetNewPasswordType = {
-  password: string
-  resetPasswordToken: string | undefined
+    password: string
+    resetPasswordToken: string | undefined
 }
